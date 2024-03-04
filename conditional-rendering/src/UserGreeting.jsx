216af5b0eb41f7d391724357cda160c072d2f6b0
LahiruHarshana <lharshana2002@gmail.com>
2024-03-04 13:44:39 +0530

    updated

--- conditional-rendering/src/UserGreeting.jsx
+++ conditional-rendering/src/UserGreeting.jsx
@@ -1,6 +1,6 @@
 function UserGreeting(props) {
 
-    return (props.isLoggedIn) ? <h1 className="welcome-massage">Welcome back, {props.username}!</h1> : <h1 className="error-massage">Welcome, Guest!</h1>
+    return (props.isLoggedIn) ? <h1 className="welcome-massage">Welcome back, {props.username}!</h1> : <h1 className="error-massage">Please SingUp!</h1>
 }
 
 export default UserGreeting;
