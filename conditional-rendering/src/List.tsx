2d4ea61ffdd4e7dbd7e9b828385a03231cb7405f
LahiruHarshana <lharshana2002@gmail.com>
2024-03-04 14:53:17 +0530

    updated

--- conditional-rendering/src/List.tsx
+++ conditional-rendering/src/List.tsx
@@ -1,7 +1,7 @@
 function List(){
     const fruits = [{name : "apple",calories : 100},
                     {name : "banana" ,calories : 200},
-                    {"orange"}];
+                    {name : "orange",calories : 150},];
 
     const listItems = fruits.map(fruit => <li>{fruit}</li>);
 
