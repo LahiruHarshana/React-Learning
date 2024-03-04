7bec8209bcabf9d8aa537dd74a5a54ceba8577e3
LahiruHarshana <lharshana2002@gmail.com>
2024-03-04 14:37:26 +0530

    updated

--- conditional-rendering/src/UserGreeting.jsx
+++ conditional-rendering/src/UserGreeting.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 function UserGreeting(props) {
 
     const welcomeMassage = <h1 className="welcome-massage">Welcome back, {props.username}!</h1>
@@ -6,6 +7,11 @@ function UserGreeting(props) {
 
     return (props.isLoggedIn ? welcomeMassage: erroMassage);
 
+}
+
+UserGreeting.prototype = {
+    isLoggedIn: PropTypes.bool,
+    username: PropTypes.string
 }
 
 export default UserGreeting;
