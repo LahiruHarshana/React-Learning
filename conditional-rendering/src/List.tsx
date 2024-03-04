ce10e92fc132ab40dfe500789401bf368f1bd86c
LahiruHarshana <lharshana2002@gmail.com>
2024-03-04 14:47:22 +0530

    updated

--- conditional-rendering/src/List.tsx
+++ conditional-rendering/src/List.tsx
@@ -1,6 +1,9 @@
 function List(){
     const fruits = ['apple', 'banana', 'orange'];
-    return(fruits);
-}
+
+    const listItems = fruits.map(fruit => <li>fruit</li>);
+
+    return(<ul>{listItems}</ul>);
+};
 
 export default List;
