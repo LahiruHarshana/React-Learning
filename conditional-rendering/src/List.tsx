a51df0edb473266c209119f8d2840785b8ff4d89
LahiruHarshana <lharshana2002@gmail.com>
2024-03-04 14:54:25 +0530

    updated

--- conditional-rendering/src/List.tsx
+++ conditional-rendering/src/List.tsx
@@ -1,9 +1,11 @@
 function List(){
     const fruits = [{name : "apple",calories : 100},
                     {name : "banana" ,calories : 200},
-                    {name : "orange",calories : 150},];
+                    {name : "orange",calories : 150},
+                    {name : "mango",calories : 300},
+                    {name : "grapes",calories : 50}];
 
-    const listItems = fruits.map(fruit => <li>{fruit}</li>);
+    const listItems = fruits.map(fruit => <li>{fruit.name}</li>);
 
     return(<ol>{listItems}</ol>);
 };
